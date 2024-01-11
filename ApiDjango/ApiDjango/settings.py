53fd4fce8653e63006c312cdf03f8cfb92399bf0
TadeoRosales <2019452127@teschi.edu.mx>
2024-01-11 01:30:28 -0600

    holi

--- ApiDjango/ApiDjango/settings.py
+++ ApiDjango/ApiDjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v6*mq+^c=r949v45k%bx0_3f)zqx-szn1^ja(iuliyq5(s9$5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
